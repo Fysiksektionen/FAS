5bc821d3f8d315d75c1e681405895e664f4a5a2a
Acksell <axelen@kth.se>
2020-01-29 19:33:21 +0100

    Authentication using CAS module. Sessions stored with mongo.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,20 +1,64 @@
 import express from "express"
-import * as apiController from "./controllers/api"
+
+/* Middleware and controllers */
 import cors from "cors"
+import bodyParser from "body-parser"
+import session from "express-session"
 
+import * as apiController from "./controllers/api"
+
+import CAS from "./controllers/auth"
+import { CASOptionsDev, CASOptionsPro} from "./lib/casOptions"
+
+
+/* Session store */
+const MongoStore = require('connect-mongo')(session)
+const store = new MongoStore({
+    url: "mongodb://localhost:27017/FAS",
+    // TODO: re-enable this when
+    // https://github.com/jdesboeufs/connect-mongo/issues/277 is fixed:
+    // mongooseConnection: db
+});
+
+
+/* Initialise app */
 const app = express()
+app.set("port", process.env.PORT || 8080)
 
-// app.get("env") reads the NODE_ENV environment variable, uses 'development' if not found
-if (app.get("env") === 'development') {
+/* Set up a session store, exposes req.session. */
+app.use( session({
+    secret            : 'super secret key',
+    resave            : false,
+    saveUninitialized : false,
+    store             : store
+}));
+
+/* Middleware setup */
+
+// exposes response attributes on req.body */
+app.use(bodyParser.json())
+
+let casAuth; // initialise depending on environment below
+
+if (app.get("env") === 'development') { 
+    // app.get("env") reads the NODE_ENV environment 
+    // variable, uses 'development' if not found
     const corsOptions = {
         origin: 'http://localhost:3000',  // react frontend during development
         optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
     }
     app.use(cors(corsOptions))
+    casAuth = CAS(CASOptionsDev) // development settings
+}
+else{
+    casAuth = CAS(CASOptionsPro) // production settings
 }
 
-app.set("port", process.env.PORT || 8080)
-
+/* API */
 app.get("/api/groups", apiController.getGroups)
+
+/* Other routes */
+// just to test
+app.get("/", casAuth.bounce, (req,res) => res.json({name:req.session['cas_user']}))
 
 export default app
---
+++ server/src/controllers/auth.ts
@@ -0,0 +1,23 @@
+const CASAuthentication = require('../lib/CASAuth')
+import { CASOptionsType } from '../lib/casOptions';
+
+
+/* CAS Authentication functions
+
+Middleware:
+-  cas.bounce: Unauthenticated clients will be redirected to the CAS login and then back to
+              the route once authenticated.
+-  cas.block:  Unauthenticated clients will receive a 401 Unauthorized response instead of data.
+
+Endpoint functions:
+-  cas.bounce_redirect: 
+              Unauthenticated clients will be redirected to the CAS login and then to the
+              provided "redirectTo" query parameter once authenticated (only used in login flow).
+  
+-  cas.logout: De-authenticate the client with the Express server and then            
+              redirect the client to the CAS logout page.
+*/
+const CAS = (options: CASOptionsType) => new CASAuthentication(options)
+
+
+export default CAS;

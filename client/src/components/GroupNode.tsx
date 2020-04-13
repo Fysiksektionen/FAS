0337af8b815dc6a2594d6a701fbfb25cd59b0c6d
Dodden <david.bardvall@gmail.com>
2020-04-13 12:42:03 +0200

    Group caching implemented

--- client/src/components/GroupNode.tsx
+++ client/src/components/GroupNode.tsx
@@ -9,7 +9,7 @@ import ExpandButton from './ExpandButton';
 // renders a single group in the expandable list/tree.
 export const GroupNode: React.FC<GroupNodeResponse> = (props: GroupNodeResponse) => {
     const [expanded, setExpanded] = useState(false)
-    const childrenArray = props.children ? props.children : []
+    const childrenArray = props.subGroups ? props.subGroups : []
     return (
         <div>
             <li className="groupnode" onClick={() => {setExpanded(!expanded)}}>
@@ -20,7 +20,7 @@ export const GroupNode: React.FC<GroupNodeResponse> = (props: GroupNodeResponse)
             render them in a list (giving an indentation)*/}
             {expanded && childrenArray && childrenArray.length ? 
             <ul>
-                {childrenArray.map((groupNode:GroupNodeResponse) => <GroupNode {...groupNode}/>)}
+                {/* {childrenArray.map((groupNode:GroupNodeResponse) => <GroupNode {...groupNode}/>)} */}
             </ul>
             : null}
         </div>
--- server/src/controllers/DirectoryApi.ts
+++ server/src/controllers/DirectoryApi.ts
@@ -2,10 +2,11 @@
 import { admin_directory_v1 } from 'googleapis';
 import { as } from './asyncUtil';
 import { basicDict } from '../../../shared/types/common';
+import { GroupNodeResponse, User } from '../../../shared/types/GroupNode';
 
 type cGroup = admin_directory_v1.Schema$Group & {
-    subGroups?: string[];
-    users?: string[];
+    subGroups: string[];
+    users: string[];
 }
 type cUser = admin_directory_v1.Schema$Member
 
@@ -13,6 +14,7 @@ export default class DirectoryApi extends admin_directory_v1.Admin {
     
     private Cgroups: {[id: string]: cGroup};
     private Cusers: {[id: string]: cUser};
+    private cached: boolean = false;
     private readonly def = {
         domain: this.domain,
         maxResults: 200,
@@ -28,30 +30,37 @@ export default class DirectoryApi extends admin_directory_v1.Admin {
      * Returns a JSON map of all users and groups in the G Suite organizaion.
      * @param opts 
      */
-    public async getMap(opts?: basicDict) {
+    public async getMap(opts?: basicDict, noCache = false) {
         opts = {...this.def, ...opts};
 
-        const [err, groups] = await as(this.listGroups(opts));
-        if (err) return Promise.reject(err);
+        if (!this.cached || noCache) {
+            const [err, groups] = await as(this.listGroups(opts));
+            if (err) return Promise.reject(err);
 
-        const memberPromises = groups.map(async group => {
-            const [err2, members] = await as(this.listMembers(group.id));
-            if (err2) return Promise.reject(err2);
-            
-            if (members) {
+            const memberPromises = groups.map(async (group: Partial<cGroup>) => {
+                const [err2, members] = await as(this.listMembers(group.id));
+                if (err2 || !members) return Promise.reject(err2);
+                
                 group.subGroups = members.filter(member => member.type === 'GROUP').map(subGroup => subGroup.id);
                 group.users = members.filter(member => member.type === 'USER').map(user => user.id);
+                return Promise.resolve(group as cGroup);
+            });
+            const [errAll, mappedGroups] = await as(Promise.all(memberPromises));
+            if (errAll) return Promise.reject({mesg: "listMembers", error: errAll});
+            
+            for (const group of mappedGroups) {
+                this.Cgroups[group.id] = group;
             }
-        });
-        const [errAll, ] = await as(Promise.all(memberPromises));
-        if (errAll) return Promise.reject({mesg: "listMembers", error: errAll});
+            this.cached = true;
+        }
+
         return {
-            groups: this.Cgroups,
+            groups: this.Cgroups as {[id: string]: GroupNodeResponse},
             users: {} // Add if needed
         };
     }
 
-    public async listGroups(opts?: basicDict) : Promise<cGroup[]> {
+    public async listGroups(opts?: basicDict) : Promise<admin_directory_v1.Schema$Group[]> {
         opts = {...this.def, ...opts};
 
         const [err, res] = await as(this.groups.list(opts));
@@ -59,9 +68,6 @@ export default class DirectoryApi extends admin_directory_v1.Admin {
 
         const groups = res.data.groups;
 
-        for (const group of groups) {
-            this.Cgroups[group.id] = group;
-        }
         if (res.data.nextPageToken) {
             const [err, nextGroups] = await as(this.listGroups({...opts, pageToken: res.data.nextPageToken}));
             if (err) return Promise.reject(err);
@@ -83,7 +89,7 @@ export default class DirectoryApi extends admin_directory_v1.Admin {
             if (err) return Promise.reject(err);
             members.push(...nextMembers);
         }
-        return members;
+        return members || [];
     }
 
     public async getUsers(groupKey: string, opts?: basicDict) {

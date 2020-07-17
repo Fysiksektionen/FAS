9abe0c748cac693f568a2511cd980504aafcaa77
morriser <morriser@kth.se>
2020-07-17 12:14:49 +0200

    Updated App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import { User, Group, Member, GroupFilled, DirectoryMap } from '../../shared/types/GroupNode';
 
 import Frontpage from './components/Frontpage';
 import Dashboard from './components/Dashboard';
 
 import './App.css';
+
+
+export enum httpMethod {get = "get", post = "post", update = "update", delete = "delete"};
+
+export async function APIRequest(APICallName: string, method: httpMethod, body?: string) {
+    const baseURI = process.env.FAS_BASE_URI || "http://localhost:8080";
+    const resourceUrl = baseURI + '/api/' + APICallName;
+    const response = await fetch(resourceUrl, {
+        method: method,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: body
+    });
+    if (response.status === 401) return Promise.reject(new Error("Unauthorized"));
+    return await response.json();
+};
+
+export function getGroupFilled(map: DirectoryMap, group: Group): GroupFilled  {
+
+    const subGroups: GroupFilled[] = [];
+    const users: User[] = [];
+    const externalUsers: Member[] = [];
+
+    group.users.map(member => {
+        users.push(map.users[member.id]);
+    });
+
+    group.subGroups.map(member => {
+        subGroups.push(getGroupFilled(map, map.groups[member.id]))
+    });
+
+    const groupFilled: GroupFilled = {...group, subGroups, users};
+    return groupFilled;
+};
 
 
 
@@ -14,16 +50,14 @@ const App: React.FC = () => {
     const [loggedin, setLoggedin] = useState(false);
 
     useEffect(() => {
+        // Local storage used as buffer as the /api/me request is async.
         if(window.sessionStorage.getItem(key) === "true") {
             setLoggedin(true);
         } else {
             setLoggedin(false);
         }
 
-        const baseURI = process.env.FAS_BASE_URI || "http://localhost:8080"
-        const resourceUrl = baseURI + '/api/me'
-        fetch(resourceUrl)
-        .then(response => response.json())
+        APIRequest('me', httpMethod.get)
         .then(response => {
             if(response.authenticated) {
                 setLoggedin(true);
@@ -33,13 +67,17 @@ const App: React.FC = () => {
                 setLoggedin(false);
                 window.sessionStorage.setItem(key, "false");
             }
-        })
+        }).catch((e: Error) => {
+            setLoggedin(false);
+            window.sessionStorage.setItem(key, "false");
+        });
     })
 
     return (
         <div className="background">
             {
                 loggedin ? <Dashboard /> : <Frontpage />
+                //<Dashboard />
             }
         </div>
     )
